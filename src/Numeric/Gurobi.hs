f188867653a2e9bd5957a4a6c60da7613ec2f452
Tom Manderson <TRManderson@gmail.com>
2016-09-29 00:18:22 +1000

    loadEnv now exists in Haskell-land

--- src/Numeric/Gurobi.hs
+++ src/Numeric/Gurobi.hs
@@ -1,5 +1,29 @@
 module Numeric.Gurobi where
-import BasicPrelude
+import Foreign
+import Foreign.C
+import Numeric.Gurobi.Types
 import qualified Numeric.Gurobi.C as C
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Except
 
-main = ()
+
+withFinalPtr :: Storable a => a -> (Ptr a -> IO b) -> IO b
+withFinalPtr def transformation = do
+  ptr <- new def
+  fp <- newForeignPtr finalizerFree ptr
+  withForeignPtr fp transformation
+
+
+loadEnv :: String -> Gurobi Env
+loadEnv logFileName = do
+  (cEnv, (CInt status)) <- lift . withCString logFileName $ \fname -> do
+    withFinalPtr nullPtr $ \h -> do
+      status <- C.loadEnv h fname
+      peekResult <- maybePeek (peek) h
+      return (peekResult, status)
+
+  case status of
+    0 -> case cEnv of
+      Just env -> return . Env $ env
+      Nothing -> throwE NotImplementedError
+    _ -> throwE NotImplementedError
--- src/Numeric/Gurobi/Types.hs
+++ src/Numeric/Gurobi/Types.hs
@@ -1,7 +1,47 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Numeric.Gurobi.Types where
 import BasicPrelude
+import Numeric.Gurobi.C (ModelHandle, EnvHandle)
 import Control.Monad.Trans.Except
+
+
+newtype Model = Model { _unModel :: ModelHandle}
+newtype Env = Env { _unEnv :: EnvHandle}
+
+data GurobiError = OutOfMemoryError
+                 | NullArgumentError
+                 | InvalidArgumentError
+                 | UnknownAttributeError
+                 | DataNotAvailableError
+                 | IndexOutOfRangeError
+                 | UnknownParameterError
+                 | ValueOutOfRangeError
+                 | NoLicenseError
+                 | SizeLimitExceededError
+                 | CallbackError
+                 | FileReadError
+                 | FileWriteError
+                 | NumericError
+                 | IISNotInfeasibleError
+                 | NotForMIPError
+                 | OptimizationInProgressError
+                 | DuplicatesError
+                 | NodeFileError
+                 | QNotPSDError
+                 | QCPEqualityConstraintError
+                 | NetworkError
+                 | JobRejectedError
+                 | NotSupportedError
+                 | Exceed2BNonZerosError
+                 | InvalidPiecewiseObjError
+                 | NotInModelError
+                 | FailedToCreateModelError
+                 | GurobiInternalError
+                 | NotImplementedError
+
+
+type Gurobi a = ExceptT GurobiError IO a
+
 
 data VariableType = Continuous | Binary | Integer | SemiContinuous | SemiInteger
 data ObjectiveSense = Minimize | Maximize
